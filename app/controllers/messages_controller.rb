fb8b399d21191f7bf4fd91010334a82167934243
yuuuuuko <phyu10078@gmail.com>
2020-07-06 18:01:14 +0000

    add column

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -52,7 +52,7 @@ class MessagesController < ApplicationController
   
   # Strong Parameter
   def message_params
-    params.require(:message).permit(:content)
+    params.require(:message).permit(:content, :title)
   end
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_22_061207) do
+ActiveRecord::Schema.define(version: 2020_07_06_175025) do
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
 end
